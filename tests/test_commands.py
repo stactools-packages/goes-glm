c5a7fc07ba45d3aa74ca6e06dd2ea14a7fbb7a6a
Matthias Mohr <m.mohr@uni-muenster.de>
2022-07-26 17:35:37 +0200

    Add command tests #6 #13

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,12 +1,21 @@
+import json
 import os.path
+import shutil
 from tempfile import TemporaryDirectory
 from typing import Callable, List
 
-import pystac
 from click import Command, Group
+from deepdiff import DeepDiff
 from stactools.testing.cli_test import CliTestCase
 
 from stactools.goes_glm.commands import create_goesglm_command
+
+SRC_FOLDER = "./tests/data-files/"
+
+TEST_FILES = [
+    "OR_GLM-L2-LCFA_G16_s20203662359400_e20210010000004_c20210010000030",
+    "OR_GLM-L2-LCFA_G17_s20221542100000_e20221542100200_c20221542100217",
+]
 
 
 class CommandsTest(CliTestCase):
@@ -15,13 +24,11 @@ class CommandsTest(CliTestCase):
 
     def test_create_collection(self) -> None:
         with TemporaryDirectory() as tmp_dir:
-            # Run your custom create-collection command and validate
-
-            # Example:
+            src_file = os.path.join(SRC_FOLDER, "collection.json")
             destination = os.path.join(tmp_dir, "collection.json")
 
             result = self.run_command(
-                f"goes-glm create-collection {destination} --id=glm --license=license.txt"
+                f"goes-glm create-collection {destination} --start_time 2022-01-01T00:00:00Z"
             )
 
             self.assertEqual(result.exit_code, 0, msg="\n{}".format(result.output))
@@ -29,30 +36,63 @@ class CommandsTest(CliTestCase):
             jsons = [p for p in os.listdir(tmp_dir) if p.endswith(".json")]
             self.assertEqual(len(jsons), 1)
 
-            collection = pystac.read_file(destination)
-            self.assertEqual(collection.id, "glm")
-            # self.assertEqual(item.other_attr...
+            collection = {}
+            truth_collection = {}
+            with open(destination) as f:
+                collection = json.load(f)
+            with open(src_file) as f:
+                truth_collection = json.load(f)
 
-            # can't validate yet due to https://github.com/stac-utils/pystac/issues/845
-            # collection.validate()
+            self.assertEqual(collection["id"], "goes-glm")
+
+            diff = DeepDiff(
+                collection,
+                truth_collection,
+                ignore_order=True,
+                exclude_regex_paths=r"root\['links'\]\[\d+\]\['href'\]",
+            )
+            self.assertEqual(diff, {})
 
     def test_create_item(self) -> None:
-        with TemporaryDirectory() as tmp_dir:
-            # Run your custom create-item command and validate
+        for id in TEST_FILES:
+            with self.subTest(id=id):
+                with TemporaryDirectory() as tmp_dir:
+                    src_data_filename = f"{id}.nc"
+                    stac_filename = f"{id}.json"
 
-            # Example:
-            id = "OR_GLM-L2-LCFA_G17_s20221542100000_e20221542100200_c20221542100217"
-            infile = f"tests/data-files/{id}.nc"
-            destination = os.path.join(tmp_dir, "item.json")
-            result = self.run_command(f"goes-glm create-item {infile} {destination}")
-            self.assertEqual(result.exit_code, 0, msg="\n{}".format(result.output))
+                    src_collection = os.path.join(SRC_FOLDER, "collection.json")
+                    src_data_file = os.path.join(SRC_FOLDER, src_data_filename)
+                    dest_data_file = os.path.join(tmp_dir, src_data_filename)
+                    shutil.copyfile(src_data_file, dest_data_file)
 
-            jsons = [p for p in os.listdir(tmp_dir) if p.endswith(".json")]
-            self.assertEqual(len(jsons), 1)
+                    src_stac = os.path.join(SRC_FOLDER, stac_filename)
+                    dest_stac = os.path.join(tmp_dir, stac_filename)
 
-            item = pystac.read_file(destination)
-            self.assertEqual(item.id, id)
-            # self.assertEqual(item.other_attr...
+                    result = self.run_command(
+                        f"goes-glm create-item {dest_data_file} {dest_stac} "
+                        f"--collection {src_collection}"
+                    )
+                    self.assertEqual(
+                        result.exit_code, 0, msg="\n{}".format(result.output)
+                    )
 
-            # can't validate yet due to https://github.com/stac-utils/pystac/issues/845
-            # item.validate()
+                    files = os.listdir(tmp_dir)
+                    jsons = [p for p in files if p.endswith(".json")]
+                    self.assertEqual(len(jsons), 1)
+
+                    item = {}
+                    truth_item = {}
+                    with open(dest_stac) as f:
+                        item = json.load(f)
+                    with open(src_stac) as f:
+                        truth_item = json.load(f)
+
+                    self.assertEqual(item["id"], id)
+
+                    diff = DeepDiff(
+                        item,
+                        truth_item,
+                        ignore_order=True,
+                        exclude_regex_paths=r"root\['(assets|links)'\]\[[\w']+\]\['href'\]",
+                    )
+                    self.assertEqual(diff, {})
