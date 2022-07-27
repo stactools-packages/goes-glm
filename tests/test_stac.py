212806c950d335a65f71298ae50afd06f961d8b0
Matthias Mohr <m.mohr@uni-muenster.de>
2022-07-27 14:39:33 +0200

    Add tests #6

--- tests/test_stac.py
+++ tests/test_stac.py
@@ -67,8 +67,6 @@ class StacTest(unittest.TestCase):
                 nogeoparquet: bool = test_data["nogeoparquet"]
                 nonetcdf: bool = test_data["nonetcdf"]
 
-                # Write tests for each for the creation of a STAC Collection
-                # Create the STAC Collection...
                 collection = stac.create_collection(LICENSE, **test_data)
                 collection.set_self_href("")
                 collection.validate()
@@ -76,8 +74,29 @@ class StacTest(unittest.TestCase):
 
                 self.assertEqual(collection.id, id)
 
+                self.assertEqual(collection_dict["sci:doi"], "10.7289/V5KH0KK6")
+                self.assertTrue("sci:citation" in collection_dict)
+
+                self.assertTrue("summaries" in collection_dict)
+                summaries = collection_dict["summaries"]
+                self.assertEqual(summaries["mission"], ["GOES"])
+                self.assertEqual(summaries["constellation"], ["GOES"])
+                self.assertEqual(summaries["platform"], ["GOES-16", "GOES-17"])
+                self.assertEqual(summaries["instruments"], ["FM1", "FM2"])
+                self.assertEqual(summaries["gsd"], [8000])
+                self.assertEqual(summaries["processing:level"], ["L2"])
+                self.assertEqual(summaries["goes:image-type"], ["FULL DISK"])
+                self.assertEqual(summaries["goes:orbital-slot"], ["West", "East"])
+
                 self.assertTrue("item_assets" in collection_dict)
                 assets: Dict[str, Dict[str, Any]] = collection_dict["item_assets"]
+
+                asset_count = 4
+                if nogeoparquet:
+                    asset_count -= 3
+                if nonetcdf:
+                    asset_count -= 1
+                self.assertEqual(len(assets), asset_count)
 
                 # Check geoparquet assets
                 for key in PARQUET_KEYS:
@@ -85,6 +104,7 @@ class StacTest(unittest.TestCase):
                     if not nogeoparquet:
                         asset = assets[key]
                         self.assertEqual(asset["type"], PARQUET_MEDIA_TYPE)
+                        self.assertEqual(asset["table:primary_geometry"], "geometry")
 
                 # Check netCDF asset
                 if nonetcdf:
@@ -93,49 +113,123 @@ class StacTest(unittest.TestCase):
                     self.assertTrue("netcdf" in assets)
                     asset = assets["netcdf"]
                     self.assertEqual(asset["type"], NETCDF_MEDIA_TYPE)
-
-                # todo: more tests #6
+                    self.assertFalse("table:primary_geometry" in asset)
 
     def test_create_item(self) -> None:
         for test_data in TEST_ITEMS:
             with self.subTest(test_data=test_data):
                 id: str = test_data["id"]
-                del test_data["id"]
                 nogeoparquet: bool = test_data["nogeoparquet"]
                 nonetcdf: bool = test_data["nonetcdf"]
+                collection: Optional[Collection] = None
+                if "collection" in test_data:
+                    collection = Collection.from_file(test_data["collection"])
 
                 item: Optional[Item] = None
                 with TemporaryDirectory() as tmp_dir:
                     src_data_file = os.path.join("./tests/data-files/", f"{id}.nc")
                     dest_data_file = os.path.join(tmp_dir, f"{id}.nc")
                     shutil.copyfile(src_data_file, dest_data_file)
-                    test_data["asset_href"] = dest_data_file
 
-                    if "collection" in test_data:
-                        test_data["collection"] = Collection.from_file(
-                            test_data["collection"]
-                        )
+                    del test_data["id"]
+                    test_data["asset_href"] = dest_data_file
+                    test_data["collection"] = collection
 
                     item = stac.create_item(**test_data)
                     item.validate()
-                    # item_dict = collection.to_dict()
 
-                    self.assertIsNotNone(item)
-                    self.assertEqual(item.id, id)
+                if id.find("_G16_") != -1:
+                    platform = 16
+                elif id.find("_G17_") != -1:
+                    platform = 17
+                else:
+                    platform = 18
 
-                    # Check geoparquet assets
-                    for key in PARQUET_KEYS:
-                        self.assertEqual(key in item.assets, not nogeoparquet)
-                        if not nogeoparquet:
-                            asset = item.assets[key].to_dict()
-                            self.assertEqual(asset["type"], PARQUET_MEDIA_TYPE)
+                self.assertIsNotNone(item)
+                self.assertEqual(item.id, id)
+                if collection is not None:
+                    self.assertEqual(item.collection_id, collection.id)
+                else:
+                    self.assertIsNone(item.collection_id)
 
-                    # Check netCDF asset
-                    if nonetcdf:
-                        self.assertFalse("netcdf" in item.assets)
-                    else:
-                        self.assertTrue("netcdf" in item.assets)
-                        asset = item.assets["netcdf"].to_dict()
-                        self.assertEqual(asset["type"], NETCDF_MEDIA_TYPE)
+                self.assertTrue("datetime" in item.properties)
+                self.assertTrue("start_datetime" in item.properties)
+                self.assertTrue("end_datetime" in item.properties)
+                self.assertEqual(item.properties["mission"], "GOES")
+                self.assertEqual(item.properties["constellation"], "GOES")
+                self.assertEqual(item.properties["platform"], f"GOES-{platform}")
+                instrument = platform - 15
+                self.assertEqual(item.properties["instruments"], [f"FM{instrument}"])
+                self.assertEqual(item.properties["gsd"], 8000)
+                self.assertEqual(item.properties["goes:system-environment"], "OR")
+                self.assertEqual(item.properties["goes:image-type"], "FULL DISK")
+                if (
+                    id
+                    == "OR_GLM-L2-LCFA_G16_s20203662359400_e20210010000004_c20210010000030"
+                ):
+                    self.assertEqual(item.properties["goes:orbital-slot"], "East")
+                elif (
+                    id
+                    == "OR_GLM-L2-LCFA_G17_s20221542100000_e20221542100200_c20221542100217"
+                ):
+                    self.assertEqual(item.properties["goes:orbital-slot"], "West")
+                else:
+                    self.assertTrue("goes:orbital-slot" in item.properties)
+                self.assertEqual(item.properties["proj:epsg"], 4326)
+                self.assertEqual(item.properties["processing:level"], "L2")
 
-                    # todo: more tests #6
+                asset_count = 4
+                if nogeoparquet:
+                    asset_count -= 3
+                if nonetcdf:
+                    asset_count -= 1
+                self.assertEqual(len(item.assets), asset_count)
+
+                # Check geoparquet assets
+                for key in PARQUET_KEYS:
+                    self.assertEqual(key in item.assets, not nogeoparquet)
+                    if not nogeoparquet:
+                        asset = item.assets[key].to_dict()
+                        self.assertEqual(asset["type"], PARQUET_MEDIA_TYPE)
+                        self.assertTrue("title" in asset)
+                        self.assertFalse("created" in asset)
+                        self.assertFalse("cube:dimensions" in asset)
+                        self.assertFalse("cube:variables" in asset)
+                        self.assertTrue("table:row_count" in asset)
+                        self.assertEqual(asset["table:primary_geometry"], "geometry")
+                        self.assertTrue("table:columns" in asset)
+                        hasGeometryCol = False
+                        for col in asset["table:columns"]:
+                            self.assertTrue("name" in col)
+                            self.assertTrue("type" in col)
+                            if col["name"] == "geometry":
+                                hasGeometryCol = True
+                        self.assertTrue(hasGeometryCol)
+
+                # Check netCDF asset
+                if nonetcdf:
+                    self.assertFalse("netcdf" in item.assets)
+                else:
+                    self.assertTrue("netcdf" in item.assets)
+                    asset = item.assets["netcdf"].to_dict()
+                    self.assertEqual(asset["type"], NETCDF_MEDIA_TYPE)
+                    self.assertTrue("title" in asset)
+                    self.assertTrue("created" in asset)
+                    self.assertFalse("table:row_count" in asset)
+                    self.assertFalse("table:columns" in asset)
+                    self.assertFalse("table:primary_geometry" in asset)
+
+                    self.assertTrue("cube:dimensions" in asset)
+                    dims = asset["cube:dimensions"]
+                    self.assertEqual(len(dims), 6)
+                    for dim in dims.values():
+                        self.assertTrue("type" in dim)
+                        self.assertTrue("extent" in dim)
+
+                    self.assertTrue("cube:variables" in asset)
+                    vars = asset["cube:variables"]
+                    self.assertEqual(len(vars), 48)
+                    for var in vars.values():
+                        self.assertTrue("dimensions" in var)
+                        self.assertTrue("type" in var)
+                        self.assertTrue("description" in var)
