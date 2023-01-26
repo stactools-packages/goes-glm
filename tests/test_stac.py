b59f0168606186a3ce4a6bd629d6aef1e390f89f
Pete Gadomski <pete.gadomski@gmail.com>
2023-01-26 13:09:59 -0700

    refactor, tests: use pytest syntax in test_stac.py
    This makes it easer to isolate a test, since they take so long to run.

--- tests/test_stac.py
+++ tests/test_stac.py
@@ -1,16 +1,17 @@
 import os.path
 import shutil
-import unittest
-
-# from datetime import datetime, timezone
 from tempfile import TemporaryDirectory
 from typing import Any, Dict, List, Optional
 
+import pytest
 from pystac import Collection, Item
 
 from stactools.goes_glm import stac
 
-LICENSE = "https://www.ncei.noaa.gov/access/metadata/landing-page/bin/iso?id=gov.noaa.ncdc:C01527"
+LICENSE = (
+    "https://www.ncei.noaa.gov/access/metadata/landing-page/bin/iso"
+    "?id=gov.noaa.ncdc:C01527"
+)
 THUMBNAIL = "https://example.com/thumb.png"
 
 PARQUET_MEDIA_TYPE = "application/x-parquet"
@@ -79,199 +80,185 @@ TEST_ITEMS: List[Dict[str, Any]] = [
 ]
 
 
-class StacTest(unittest.TestCase):
-    def test_create_collection(self) -> None:
-        for test_data in TEST_COLLECTIONS:
-            with self.subTest(test_data=test_data):
-                id: str = test_data["id"] if "id" in test_data else "goes-glm"
-                nogeoparquet: bool = (
-                    test_data["nogeoparquet"] if "nogeoparquet" in test_data else False
-                )
-                nonetcdf: bool = (
-                    test_data["nonetcdf"] if "nonetcdf" in test_data else False
-                )
+@pytest.mark.parametrize("options", TEST_COLLECTIONS)
+def test_create_collection(options: Dict[str, Any]) -> None:
+    id: str = options["id"] if "id" in options else "goes-glm"
+    nogeoparquet: bool = options["nogeoparquet"] if "nogeoparquet" in options else False
+    nonetcdf: bool = options["nonetcdf"] if "nonetcdf" in options else False
 
-                collection = stac.create_collection(LICENSE, **test_data)
-                collection.set_self_href("")
-                collection.validate()
-                collection_dict = collection.to_dict()
+    collection = stac.create_collection(LICENSE, **options)
+    collection.set_self_href("")
+    collection.validate()
+    collection_dict = collection.to_dict()
 
-                self.assertEqual(collection.id, id)
+    assert collection.id == id
 
-                self.assertEqual(collection_dict["sci:doi"], "10.7289/V5KH0KK6")
-                self.assertTrue("sci:citation" in collection_dict)
+    assert collection_dict["sci:doi"] == "10.7289/V5KH0KK6"
+    assert "sci:citation" in collection_dict
 
-                self.assertTrue("summaries" in collection_dict)
-                summaries = collection_dict["summaries"]
-                self.assertEqual(summaries["mission"], ["GOES"])
-                self.assertEqual(summaries["constellation"], ["GOES"])
-                self.assertEqual(summaries["platform"], ["GOES-16", "GOES-17"])
-                self.assertEqual(summaries["instruments"], ["FM1", "FM2"])
-                self.assertEqual(summaries["gsd"], [8000])
-                self.assertEqual(summaries["processing:level"], ["L2"])
-                self.assertEqual(
-                    summaries["goes:orbital_slot"], ["West", "East", "Test"]
-                )
+    assert "summaries" in collection_dict
+    summaries = collection_dict["summaries"]
+    assert summaries["mission"] == ["GOES"]
+    assert summaries["constellation"] == ["GOES"]
+    assert summaries["platform"] == ["GOES-16", "GOES-17"]
+    assert summaries["instruments"] == ["FM1", "FM2"]
+    assert summaries["gsd"] == [8000]
+    assert summaries["processing:level"] == ["L2"]
+    assert summaries["goes:orbital_slot"] == ["West", "East", "Test"]
 
-                self.assertTrue("item_assets" in collection_dict)
-                assets: Dict[str, Dict[str, Any]] = collection_dict["item_assets"]
+    assert "item_assets" in collection_dict
+    assets: Dict[str, Dict[str, Any]] = collection_dict["item_assets"]
 
-                asset_count = 4
-                if nogeoparquet:
-                    asset_count -= 3
-                if nonetcdf:
-                    asset_count -= 1
-                self.assertEqual(len(assets), asset_count)
+    asset_count = 4
+    if nogeoparquet:
+        asset_count -= 3
+    if nonetcdf:
+        asset_count -= 1
+    assert len(assets) == asset_count
 
-                # Check geoparquet assets
-                for key in PARQUET_KEYS:
-                    self.assertEqual(key in assets, not nogeoparquet)
-                    if not nogeoparquet:
-                        asset = assets[key]
-                        self.assertEqual(asset["type"], PARQUET_MEDIA_TYPE)
-                        self.assertEqual(asset["table:primary_geometry"], "geometry")
+    # Check geoparquet assets
+    for key in PARQUET_KEYS:
+        assert (key in assets) == (not nogeoparquet)
+        if not nogeoparquet:
+            asset = assets[key]
+            assert asset["type"] == PARQUET_MEDIA_TYPE
+            assert asset["table:primary_geometry"] == "geometry"
 
-                # Check netCDF asset
-                if nonetcdf:
-                    self.assertFalse("netcdf" in assets)
-                else:
-                    self.assertTrue("netcdf" in assets)
-                    asset = assets["netcdf"]
-                    self.assertEqual(asset["type"], NETCDF_MEDIA_TYPE)
-                    self.assertFalse("table:primary_geometry" in asset)
+    # Check netCDF asset
+    if nonetcdf:
+        assert "netcdf" not in assets
+    else:
+        assert "netcdf" in assets
+        asset = assets["netcdf"]
+        assert asset["type"] == NETCDF_MEDIA_TYPE
+        assert "table:primary_geometry" not in asset
 
-    def test_create_item(self) -> None:
-        for test_data in TEST_ITEMS:
-            with self.subTest(test_data=test_data):
-                name: str = test_data["name"]
-                id: str = test_data["id"] if "id" in test_data else name
-                nogeoparquet: bool = (
-                    test_data["nogeoparquet"] if "nogeoparquet" in test_data else False
-                )
-                nonetcdf: bool = (
-                    test_data["nonetcdf"] if "nonetcdf" in test_data else False
-                )
-                appendctime: bool = (
-                    test_data["appendctime"] if "appendctime" in test_data else False
-                )
-                goes_test: bool = test_data["test"] if "test" in test_data else False
 
-                collection: Optional[Collection] = None
-                if "collection" in test_data:
-                    collection = Collection.from_file(test_data["collection"])
+@pytest.mark.parametrize("options", TEST_ITEMS)
+def test_create_item(options: Dict[str, Any]) -> None:
+    name: str = options["name"]
+    id: str = options["id"] if "id" in options else name
+    nogeoparquet: bool = options["nogeoparquet"] if "nogeoparquet" in options else False
+    nonetcdf: bool = options["nonetcdf"] if "nonetcdf" in options else False
+    appendctime: bool = options["appendctime"] if "appendctime" in options else False
+    goes_test: bool = options["test"] if "test" in options else False
 
-                item: Optional[Item] = None
-                with TemporaryDirectory() as tmp_dir:
-                    src_data_file = os.path.join("./tests/data-files/", f"{name}.nc")
-                    dest_data_file = os.path.join(tmp_dir, f"{name}.nc")
-                    shutil.copyfile(src_data_file, dest_data_file)
+    collection: Optional[Collection] = None
+    if "collection" in options:
+        collection = Collection.from_file(options["collection"])
 
-                    del test_data["name"]
-                    if "test" in test_data:
-                        del test_data["test"]
-                    if "id" in test_data:
-                        del test_data["id"]
-                    test_data["asset_href"] = dest_data_file
-                    test_data["collection"] = collection
+    item: Optional[Item] = None
+    with TemporaryDirectory() as tmp_dir:
+        src_data_file = os.path.join("./tests/data-files/", f"{name}.nc")
+        dest_data_file = os.path.join(tmp_dir, f"{name}.nc")
+        shutil.copyfile(src_data_file, dest_data_file)
 
-                    item = stac.create_item(**test_data)
-                    item.validate()
+        del options["name"]
+        if "test" in options:
+            del options["test"]
+        if "id" in options:
+            del options["id"]
+        options["asset_href"] = dest_data_file
+        options["collection"] = collection
 
-                if id.find("_G16_") != -1:
-                    platform = 16
-                elif id.find("_G17_") != -1:
-                    platform = 17
-                else:
-                    platform = 18
+        item = stac.create_item(**options)
+        item.validate()
 
-                self.assertIsNotNone(item)
-                self.assertEqual(item.id, name if appendctime else id)
-                if collection is not None:
-                    self.assertEqual(item.collection_id, collection.id)
-                else:
-                    self.assertIsNone(item.collection_id)
+    if id.find("_G16_") != -1:
+        platform = 16
+    elif id.find("_G17_") != -1:
+        platform = 17
+    else:
+        platform = 18
 
-                self.assertTrue("datetime" in item.properties)
-                self.assertTrue("start_datetime" in item.properties)
-                self.assertTrue("end_datetime" in item.properties)
-                self.assertEqual(item.properties["mission"], "GOES")
-                self.assertEqual(item.properties["constellation"], "GOES")
-                self.assertEqual(item.properties["platform"], f"GOES-{platform}")
-                instrument = platform - 15
-                self.assertEqual(item.properties["instruments"], [f"FM{instrument}"])
-                self.assertEqual(item.properties["gsd"], 8000)
-                self.assertEqual(item.properties["goes:system_environment"], "OR")
-                if goes_test:
-                    self.assertEqual(item.properties["goes:orbital_slot"], "Test")
-                elif platform == 16:
-                    self.assertEqual(item.properties["goes:orbital_slot"], "East")
-                elif platform == 17:
-                    self.assertEqual(item.properties["goes:orbital_slot"], "West")
-                else:
-                    self.assertTrue("goes:orbital_slot" in item.properties)
-                self.assertEqual(item.properties["proj:epsg"], 4326)
-                self.assertEqual(item.properties["processing:level"], "L2")
+    assert item is not None
+    assert item.id == name if appendctime else id
+    if collection is not None:
+        assert item.collection_id == collection.id
+    else:
+        assert item.collection_id is None
 
-                asset_count = 4
-                if nogeoparquet:
-                    asset_count -= 3
-                if nonetcdf:
-                    asset_count -= 1
-                self.assertEqual(len(item.assets), asset_count)
+    assert "datetime" in item.properties
+    assert "start_datetime" in item.properties
+    assert "end_datetime" in item.properties
+    assert item.properties["mission"] == "GOES"
+    assert item.properties["constellation"] == "GOES"
+    assert item.properties["platform"] == f"GOES-{platform}"
+    instrument = platform - 15
+    assert item.properties["instruments"] == [f"FM{instrument}"]
+    assert item.properties["gsd"] == 8000
+    assert item.properties["goes:system_environment"] == "OR"
+    if goes_test:
+        assert item.properties["goes:orbital_slot"] == "Test"
+    elif platform == 16:
+        assert item.properties["goes:orbital_slot"] == "East"
+    elif platform == 17:
+        assert item.properties["goes:orbital_slot"] == "West"
+    else:
+        assert "goes:orbital_slot" in item.properties
+    assert item.properties["proj:epsg"] == 4326
+    assert item.properties["processing:level"] == "L2"
 
-                # Check geoparquet assets
-                for key in PARQUET_KEYS:
-                    self.assertEqual(key in item.assets, not nogeoparquet)
-                    if not nogeoparquet:
-                        asset = item.assets[key].to_dict()
-                        self.assertEqual(asset["type"], PARQUET_MEDIA_TYPE)
-                        self.assertTrue("title" in asset)
-                        self.assertFalse("created" in asset)
-                        self.assertFalse("cube:dimensions" in asset)
-                        self.assertFalse("cube:variables" in asset)
-                        self.assertTrue("table:row_count" in asset)
-                        self.assertEqual(asset["table:primary_geometry"], "geometry")
-                        self.assertTrue("table:columns" in asset)
-                        cols = []
-                        for col in asset["table:columns"]:
-                            self.assertTrue("name" in col)
-                            self.assertTrue("type" in col)
-                            cols.append(col["name"])
-                        # check for geometry column
-                        self.assertIn("geometry", cols)
-                        # check for datetime columns
-                        if key == "geoparquet_flashes":
-                            self.assertIn("time_of_first_event", cols)
-                            self.assertIn("time_of_last_event", cols)
-                        else:
-                            self.assertIn("time", cols)
+    asset_count = 4
+    if nogeoparquet:
+        asset_count -= 3
+    if nonetcdf:
+        asset_count -= 1
+    assert len(item.assets) == asset_count
 
-                # Check netCDF asset
-                if nonetcdf:
-                    self.assertFalse("netcdf" in item.assets)
-                else:
-                    self.assertTrue("netcdf" in item.assets)
-                    asset = item.assets["netcdf"].to_dict()
-                    self.assertEqual(asset["type"], NETCDF_MEDIA_TYPE)
-                    self.assertTrue("title" in asset)
-                    self.assertTrue("created" in asset)
-                    self.assertFalse("table:row_count" in asset)
-                    self.assertFalse("table:columns" in asset)
-                    self.assertFalse("table:primary_geometry" in asset)
+    # Check geoparquet assets
+    for key in PARQUET_KEYS:
+        assert (key in item.assets) == (not nogeoparquet)
+        if not nogeoparquet:
+            asset = item.assets[key].to_dict()
+            assert asset["type"] == PARQUET_MEDIA_TYPE
+            assert "title" in asset
+            assert "created" not in asset
+            assert "cube:dimensions" not in asset
+            assert "cube:variables" not in asset
+            assert "table:row_count" in asset
+            assert asset["table:primary_geometry"] == "geometry"
+            assert "table:columns" in asset
+            cols = []
+            for col in asset["table:columns"]:
+                assert "name" in col
+                assert "type" in col
+                cols.append(col["name"])
+            # check for geometry column
+            assert "geometry" in cols
+            # check for datetime columns
+            if key == "geoparquet_flashes":
+                assert "time_of_first_event" in cols
+                assert "time_of_last_event" in cols
+            else:
+                assert "time" in cols
 
-                    self.assertTrue("cube:dimensions" in asset)
-                    dims = asset["cube:dimensions"]
-                    self.assertEqual(len(dims), 6)
-                    for dim in dims.values():
-                        self.assertTrue("type" in dim)
-                        self.assertTrue("extent" in dim)
+    # Check netCDF asset
+    if nonetcdf:
+        assert "netcdf" not in item.assets
+    else:
+        assert "netcdf" in item.assets
+        asset = item.assets["netcdf"].to_dict()
+        assert asset["type"] == NETCDF_MEDIA_TYPE
+        assert "title" in asset
+        assert "created" in asset
+        assert "table:row_count" not in asset
+        assert "table:columns" not in asset
+        assert "table:primary_geometry" not in asset
 
-                    self.assertTrue("cube:variables" in asset)
-                    vars = asset["cube:variables"]
-                    # Some older files have 45 variables, newer ones have 48. See
-                    # https://github.com/stactools-packages/goes-glm/issues/17#issuecomment-1241875842
-                    self.assertTrue(len(vars) == 45 or len(vars) == 48)
-                    for var in vars.values():
-                        self.assertTrue("dimensions" in var)
-                        self.assertTrue("type" in var)
-                        self.assertTrue("description" in var)
+        assert "cube:dimensions" in asset
+        dims = asset["cube:dimensions"]
+        assert len(dims) == 6
+        for dim in dims.values():
+            assert "type" in dim
+            assert "extent" in dim
+
+        assert "cube:variables" in asset
+        vars = asset["cube:variables"]
+        # Some older files have 45 variables, newer ones have 48. See
+        # https://github.com/stactools-packages/goes-glm/issues/17#issuecomment-1241875842
+        assert len(vars) == 45 or len(vars) == 48
+        for var in vars.values():
+            assert "dimensions" in var
+            assert "type" in var
+            assert "description" in var
